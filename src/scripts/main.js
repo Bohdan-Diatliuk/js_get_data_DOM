75fc8940992ee61b88a7a6771f5b94deee8ce33e
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-10-03 22:07:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const getSelectorSpan = document.querySelectorAll('span.population');
+
+const numbers = [];
+
+getSelectorSpan.forEach((span) => {
+  const text = span.innerText;
+  const number = Number(text.replace(/[^\d]/g, ''));
+
+  if (!isNaN(number)) {
+    numbers.push(number);
+  }
+});
+
+const total = numbers.reduce((a, b) => a + b, 0);
+const average = total / numbers.length;
+
+const totalFormat = total.toLocaleString('en-US');
+const averageFormat = average.toLocaleString('en-US');
+
+document.querySelector('span.total-population').innerText = totalFormat;
+document.querySelector('span.average-population').innerText = averageFormat;
